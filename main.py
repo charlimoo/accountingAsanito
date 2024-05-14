893c015d57a797d02adb3db79d4bfbc9c7f2d22b
charlimoo <141790588+charlimoo@users.noreply.github.com>
2024-05-14 05:37:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -199,4 +199,4 @@ def download_file(filename):
     return send_from_directory('static/csv', filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=80, debug=True)
